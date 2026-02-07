9023da1963a8eec6e4bac2654f8aa7a6942b75bd
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-07 18:24:37 +0100

    chore: add Gradle configuration for surf-tutorial modules

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+include("surf-tutorial-api")
+include("surf-tutorial-paper")
+include("surf-tutorial-server-event")
+include("surf-tutorial-server-lobby")
---
+++ surf-tutorial-server-survival/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("dev.slne.surf.surfapi.gradle.paper-plugin")
+}
+
+dependencies {
+    api(project(":surf-tutorial-api"))
+}
+
+surfPaperPluginApi {
+    mainClass("dev.slne.surf.tutorial.server.survival.PaperMain")
+    authors.add("red")
+    foliaSupported(true)
+
+    generateLibraryLoader(false)
+}
